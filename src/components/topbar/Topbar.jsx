bd45300d2fede4455992e46d325640fa1bde2c8d
Ghadahb <ghada.dxb@gmail.com>
2022-02-28 20:42:55 +0300

    fixing-UI-and-Admin-functions

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">cafeAdmin</span>
+          <span className="logo">SayCoffee Admin</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
